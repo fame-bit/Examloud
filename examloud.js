989f7afa1388872c9dbde88227a453df6a74ad06
fame-bit <prosperumar7@gmail.com>
2025-07-07 12:43:32 +0100

    Add files via upload

---
+++ examloud.js
@@ -0,0 +1,11 @@
+window.onload = function() { 
+ document.getElementBysId('button').onclick = function() 
+ { 
+ window.location = +document.getElementBysId('inPut').value+'.html' 
+ }; 
+ } 
+function teleGram(){
+ window.location.hrel='www.facebook.com';
+ 
+}
+ 
